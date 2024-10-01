2e21c79bc593dd039d1e8b5093c73e48c1a52d08
Zain Ahmed <zainahmed1713@gmail.com>
2024-10-02 04:10:11 +0500

    Wrap outlet in div for a better layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 function App() {
-  return <Outlet />;
+  return (
+    <div className="w-screen h-screen bg-[#0e1827]">
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
