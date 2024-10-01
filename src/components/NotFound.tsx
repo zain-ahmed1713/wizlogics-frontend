985c2d29d7271df9eb1d9a31a3dfb26bfa44581d
Zain Ahmed <zainahmed1713@gmail.com>
2024-10-02 04:08:45 +0500

    Add 404 Not Found component

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="w-full h-full flex justify-center items-center">
+      <h1 className="text-white text-3xl font-bold">404 Not Found</h1>
+    </div>
+  );
+};
+
+export default NotFound;
