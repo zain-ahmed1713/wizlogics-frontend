4f9eba2dacaa85157d20a2197311ac7a1bed38b3
Zain Ahmed <zainahmed1713@gmail.com>
2024-10-03 05:59:48 +0500

    Add routes for courses and modules

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,12 @@ import Form from "./components/Form";
 import NotFound from "./components/NotFound.tsx";
 import { AuthProvider } from "./contexts/AuthContext.tsx";
 import ProtectedRoute from "./components/ProtectedRoute.tsx";
-import AdminHome from "./pages/AdminHome.tsx";
+import AdminHome from "./pages/Admin/Admin-Home/AdminHome.tsx";
+import AdminCourses from "./pages/Admin/Admin-Courses/AdminCourses.tsx";
+import CreateCourse from "./pages/Admin/Admin-Courses/Create-Course/CreateCourse.tsx";
+import ShowCourse from "./pages/Admin/Admin-Courses/Show-Course/ShowCourse.tsx";
+import ShowModule from "./pages/Admin/Admin-Courses/Show-Module/ShowModule.tsx";
+import CreateModule from "./pages/Admin/Admin-Courses/Create-Module/CreateModule.tsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -21,7 +26,17 @@ const router = createBrowserRouter(
       <Route path="/signup" element={<Form mode="SIGNUP" />} />
       <Route path="/verify/:username" element={<Form mode="VERIFY" />} />
       <Route path="/login" element={<Form mode="LOGIN" />} />
-      <Route element={<ProtectedRoute />}></Route>
+      <Route element={<ProtectedRoute />}>
+        <Route path="/admin-panel" element={<AdminHome />} />
+        <Route path="/admin-courses" element={<AdminCourses />} />
+        <Route path="/admin-courses/:courseId" element={<ShowCourse />} />
+        <Route path="/admin-create-course" element={<CreateCourse />} />
+        <Route path="/admin-modules/:moduleId" element={<ShowModule />} />
+        <Route
+          path="/admin-courses/:courseId/modules/create-module"
+          element={<CreateModule />}
+        />
+      </Route>
       <Route path="*" element={<NotFound />} />
     </Route>
   )
